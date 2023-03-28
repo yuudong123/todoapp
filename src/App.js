b8fdbd916d28317f456e2097030ac1c154a7d271
yuudong123 <1mouri81@gmail.com>
2023-03-28 17:47:10 +0900

    delete button resize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
 
+import "bootstrap/dist/css/bootstrap.min.css"; /* 누락 추가 */
 import { Badge, Button, Container, Row, Col } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
+  // 스테이트 변수 , 수정함수로만 변경가능(immutable), 직접 스테이트를 변경하려면 오류발생!!
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
+  // 여러가지 데이터값 저장 : Array, Object
   const onChange = (e) => {
     setTodo(e.target.value);
+    // console.log("입력값 : " + todo);
   };
   const onSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault(); // 기본 이벤트 전파를 막음(=화면 새로고침)
     if (todo === "" || todo.length === 0) {
+      // 입력값 없으면
       alert("할일을 입력하세요!");
     } else {
+      //todos.push(todo); // 직접 todos 배열을 조작하지만, 결국엔 수정함수로 변경
       setTodos([...todos, todo]);
-      setTodo("");
+      setTodo(""); // 입력폼 지우기
+      // console.log(todos); // 입력된 todos를 출력
     }
   };
-  const delTodo = (index) => setTodos(todos.filter((_, i) => i !== index));
+  const delTodo = (id) => setTodos(todos.filter((todo, i) => i !== id));
+
   return (
     <div className="wrap">
       <header className="App">
@@ -30,23 +38,26 @@ function App() {
           <Row>
             <Col>
               <form onSubmit={onSubmit}>
-                <Badge>할일</Badge>
+                <label htmlFor="input_todo">
+                  <Badge bg="primary">할일</Badge>
+                </label>
                 <input type="text" id="input_todo" placeholder="오늘의 중요 일정을 작성하세요" value={todo} onChange={onChange} />
-                <Button type="submit" variant="primary" size="sm">
-                  추가
+                <Button variant="primary" size="sm" type="submit">
+                  등록
                 </Button>
               </form>
             </Col>
           </Row>
           <hr />
+          {/* 결과 출력 : 주석 */}
           <Row>
-            <Col>
+            <Col xl={6}>
               <ul>
                 {todos.map((todo, index) => (
-                  <li key={index}>
+                  <li key={index} style={{ margin: "1rem 0", position: "relative" }}>
                     {todo}{" "}
-                    <Button size="sm" onClick={() => delTodo(index)}>
-                      &times;
+                    <Button variant="danger" onClick={() => delTodo(index)} style={{ position: "absolute", right: "10px", height: "100%", margin: 0, padding: 0 }}>
+                      삭제
                     </Button>
                   </li>
                 ))}
